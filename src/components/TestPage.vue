b459c36f432fcde0a76d02377d017d87306584f6
qwark97 <marcin.plata@monit24.pl>
2021-09-19 01:02:24 +0200

    router experiments

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="main">
-    home page component
+    test page component
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'HomePage',
+  name: 'TestPage',
   components: {
   }
 }
