845470175a349af31c752d6a7e090a533ca903e7
qwark97 <marcin.plata@monit24.pl>
2021-09-19 14:56:46 +0200

    introduction to using the API

---
+++ src/api/gets.js
@@ -0,0 +1,28 @@
+const URL = 'http://localhost:9999';
+
+
+async function getCategories() {
+	let response = makeRequest('/api/categories', 'GET')
+	return await response
+}
+
+async function makeRequest(endpoint, method, dataToSend={}) {
+	let url = URL + endpoint
+	let data = {
+		method: method,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}
+	if (method != 'GET') {
+		data.data = dataToSend
+	}
+	const response = await fetch(url, data)
+
+	return response.json()
+}
+
+
+export const api = {
+	getCategories,
+};
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="main">
-    home page component
+    <div class="cards">
+      <SmallCard v-for="category in categories" v-bind:key="category.name"/>
+    </div>
   </div>
 </template>
 
 <script>
+import SmallCard from '@/components/content/cards/SmallCard.vue'
+import { api } from "@/api/gets";
 
 export default {
   name: 'HomePage',
   components: {
-  }
+    SmallCard
+  },
+  data: function () {
+    return {
+      loading: true,
+      categories: [],
+    };
+  },
+  methods: {
+    getCategories: async function () {
+      const categories = await api.getCategories();
+      console.log("asd", categories)
+      this.categories = categories;
+      this.loading = false;
+    },
+  },
+  mounted: async function () {
+    await this.getCategories();
+  },
 }
 </script>
 
