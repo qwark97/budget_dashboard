f003fd6effbcec5b6bcbdd64643f3b1009af8a73
qwark97 <marcin.plata1.konta@gmail.com>
2021-09-19 20:56:41 +0200

    introduce new component: small card to present some short info

---
+++ src/components/content/cards/SmallCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="small-card">
+    <div class="card-name">
+      {{ name }}
+    </div>
+    <div class="card-value">
+      {{ value }} {{ unit }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SmallCard",
+  props: ['name', 'value', 'unit'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
