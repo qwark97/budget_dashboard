8903bea5e9bc5044a1d658e470218d6b8a3abf05
qwark97 <marcin.plata1.konta@gmail.com>
2021-09-19 20:55:43 +0200

    introduce "static" texts easy to change if someday more than polish language will be used in project

---
+++ src/descriptions/descriptions.js
@@ -0,0 +1,12 @@
+const LANG = {
+    "PL": {
+        "ALL_FREE_MONEY": "Wolne pieniądze"
+    },
+    "ENG": {
+        "ALL_FREE_MONEY": "Money left"
+    }
+}
+
+export const lang = {
+	LANG,
+};
