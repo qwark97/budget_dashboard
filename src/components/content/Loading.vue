eac062a8ee783004a0e3711005aa6d1d0aafb50e
qwark97 <marcin.plata1.konta@gmail.com>
2021-09-19 19:56:11 +0200

    add sketch of the loading component

---
+++ src/components/content/Loading.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="loading">
+    Loading...
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
