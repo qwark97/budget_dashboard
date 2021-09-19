a75d70ab48289f94cc7f58dc0753917e0da9c2b5
qwark97 <marcin.plata@monit24.pl>
2021-09-19 14:55:34 +0200

    rename component

---
+++ src/components/content/cards/SmallCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="small-card">
+    <div class="name">
+      {{ name }}
+    </div>
+    <div class="value">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SmallCard",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
