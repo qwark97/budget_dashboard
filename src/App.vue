783321420ee3dc8fad2ceece6e7f6770f832dd7f
qwark97 <marcin.plata1.konta@gmail.com>
2021-09-29 23:01:14 +0200

    figure out general layout of the page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="content">
-    <Header />
-    <Sidebar />
-    <router-view />
-    <Footer />
+  <div class="general-content">
+    <Header class="general-header" />
+    <Sidebar class="general-sidebar" />
+    <router-view class="general-middle" />
+    <Footer class="general-footer" />
   </div>
 </template>
 
@@ -30,5 +30,36 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.general-content {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: 1fr 12fr 1fr;
+  height: 100vh;
+  grid-template-areas: 
+    "sidebar header"   
+    "sidebar middle"
+    "sidebar footer"
+}
+
+.general-header {
+  grid-area: header;
+  background-color: #638931;
+}
+
+.general-sidebar {
+  grid-area: sidebar;
+  background-color: #893144;
+}
+
+.general-middle {
+  grid-area: middle;
+  background-color: #efefef;
+}
+
+.general-footer {
+  grid-area: footer;
+  background-color: #317589;
 }
 </style>
