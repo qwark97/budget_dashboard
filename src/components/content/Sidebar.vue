753ed2ae5b2b1345023f1a863aa68cb19a81ff11
qwark97 <marcin.plata1.konta@gmail.com>
2021-10-03 17:10:21 +0200

    add to sidebar simple way of navigating through the app

--- src/components/content/Sidebar.vue
+++ src/components/content/Sidebar.vue
@@ -1,14 +1,38 @@
 <template>
-<div class="sidebar"></div>
+  <div class="sidebar">
+    <div class="navigations">
+      <ul>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">
+            {{ homePage }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/add-expense">
+            {{ addExpenses }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import { lang } from "@/descriptions/descriptions";
+
 export default {
-  name: 'Sidebar',
-}
+  name: "Sidebar",
+  computed: {
+    homePage() {
+      return lang.LANG[this.$chosenLang]["HOME_PAGE"];
+    },
+    addExpenses() {
+      return lang.LANG[this.$chosenLang]["ADD_EXPENSES_PAGE"];
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
