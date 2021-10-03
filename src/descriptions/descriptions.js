1a04e916f347e95dca729e77e24ffa7c24e0fcfa
qwark97 <marcin.plata1.konta@gmail.com>
2021-10-03 17:09:25 +0200

    add some descriptions in two langs

--- src/descriptions/descriptions.js
+++ src/descriptions/descriptions.js
@@ -1,9 +1,21 @@
 const LANG = {
     "PL": {
-        "ALL_FREE_MONEY": "Wolne pieniądze"
+        "HOME_PAGE": "Strona główna",
+        "ADD_EXPENSES_PAGE": "Dodaj wydatki",
+        "ALL_FREE_MONEY": "Wolne pieniądze",
+        "ADD_OPERATION_BUTTON_TITLE": "Dodaj wydatek",
+        "ADD_OPERATION_SUCCESS_MSG": "Dodano wydatek!",
+        "ADD_OPERATION_FAIL_MSG": "Coś poszło nie tak!",
+        "ADD_OPERATION_CATEGORY_PLACEHOLDER": "Podaj kategorię",
     },
     "ENG": {
-        "ALL_FREE_MONEY": "Money left"
+        "HOME_PAGE": "Home",
+        "ADD_EXPENSES_PAGE": "Add Expenses",
+        "ALL_FREE_MONEY": "Money left",
+        "ADD_OPERATION_BUTTON_TITLE": "Add operation",
+        "ADD_OPERATION_SUCCESS_MSG": "Added operation!",
+        "ADD_OPERATION_FAIL_MSG": "Something went wrong!",
+        "ADD_OPERATION_CATEGORY_PLACEHOLDER": "Set category",
     }
 }
 
